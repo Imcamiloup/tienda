fc7cf7343c1697365fe903da54a4dcca48555d14
franravera <fraaanco.10@hotmail.com>
2024-02-27 01:54:43 -0300

    SearchBar... Franco

---
+++ src/Handlers/searchBarHandler.js
@@ -0,0 +1,17 @@
+const {searchBarController} = require('../Controller/searchBarControllers')
+
+const searchBarHandler = async (req, res) => {
+    const { typeSearch } = req.query;
+  
+    try {
+      const searchResults = await searchBarController(typeSearch);
+      res.status(200).json(searchResults);
+    } catch (error) {
+      console.error("Error searching products:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  };
+  
+  module.exports = {
+    searchBarHandler,
+  };
